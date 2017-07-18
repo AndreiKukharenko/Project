38d125f19f63f67c45232577cbd8eaa595cdbc0f
Andrei Kukharenko <andrei-kukharenko@mail.ru>
2017-07-18 20:13:41 +0300

    + folders, unified index.html (via bem), empty .js files

---
+++ 3/3.1/js/DateDisplayFormatter.js
@@ -0,0 +1,14 @@
+
+
+var userInput;
+/*
+document.getElementsByClassName("buttons__confirm")[0].addEventListener("click", function(){
+    userInput = document.getElementsByClassName("form__form-for-text");
+    document.writeln(userInput[0].value)
+});      both are runable :))           */
+
+  
+document.querySelector('.buttons__confirm').addEventListener('click', function() {
+    userInput = document.querySelector(".form__form-for-text");
+    document.writeln(userInput.value)
+}, false);
---
+++ 3/3.3/js/ArraySorter.js
@@ -0,0 +1,18 @@
+
+var arr = new Array(5, 15, 65,0, 56, 94, 77, 16, 555, 44, -5, 4, 0);
+var arr1 = [5, 15, 65,0, 56, 94, 77, 16, 555, 44, -5, 4, 0];
+
+
+document.writeln(
+    "<p>сортировка пузырьком</p> \
+    <p>Исходный массив</p>"
+)
+
+ arr.forEach(function(element){
+        document.writeln(element)
+    } )
+
+
+function func(){
+
+}
