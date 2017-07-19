5b23cb519a9af425ad6004fc1e3b8e18895df00a
Andrei Kukharenko <andrei-kukharenko@mail.ru>
2017-07-20 00:37:52 +0300

    + two sorting methods in task 3.3

--- 3/3.3/js/ArraySorter.js
+++ 3/3.3/js/ArraySorter.js
@@ -1,38 +1,40 @@
 
+var Sorter = {
 
-var arr = new Array(5, 15, 65,0, 56, 94, 77, 16, 555, 44, -5, 4, 0);
-var arr1 = [5, 15, 65, 0, 56, 94, 77, 16, 555, 44, -5, 4, 0];
-var sortedArr=0;
+    bubbleSort : function(arr){
+        let length = arr.length;
+        let temp = 0;
+        for (let index = length - 1; index >= 0 ; index--) {
+            for (let j = 0; j < index; j++) {
+                if(arr[j] > arr[j+1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }        
+        }
+        return arr;
+    },
 
-    document.writeln(
-        "<p>Initial array</p>"
-    ) 
+    insertionSort : function(arr){
+        let length = arr.length;
+        let temp = 0;
+        for (let index = 1; index < length; index++) {
+            temp = arr[index];
+            for (var j = index-1; j>=0 && arr[j] > temp; j--) {
+                arr[j+1] = arr[j];
+            }
+            arr[j+1] = temp;            
+        }
+        return arr;
+    },
 
-    arr.forEach(function(element){
-        document.writeln(element)
-    } )
-
-    document.write("<br>");
-
-    sortedArr = arr.sort(sortNumber);
-    function sortNumber(a,b) {
-    return a - b;
+    selectionSort : function(arr){
+        let length = arr.length;
+        let temp = 0;
+        for (let index = 0; index < length-1; index++) {
+            //// COMPLETE
+        }
     }
 
-    function output(sortedArray){
-        var arrAsString = sortedArray.join(";  ");
-        getClass(".result__output").innerHTML = arrAsString;
-    }
-
-
-    /** for convenience) 
-    accepts receivedClass as ".className"*/
-    var getClass = function(receivedClass){
-        var dd =  document.querySelector(receivedClass);
-        console.log(dd);
-        return dd;
-    }
-    
-
-    document.writeln("<p>bubble sort</p>");
-    output(sortedArr);
+}
---
+++ 3/3.3/js/script.js
@@ -0,0 +1,37 @@
+
+var arr = new Array(5, 15, 65,0, 56, 94, 77, 16, 555, 44, -5, 4, 0);
+var arr1 = [5, 15, 65, 0, 56, 94, 77, 16, 555, 44, -5, 4, 0];
+var sortedArr=0;
+
+    document.writeln(
+        "<p>Initial array</p>"
+    ) 
+
+    arr.forEach(function(element){
+        document.writeln(element)
+    } )
+
+    document.write("<br>");
+
+ /*   sortedArr = arr.sort(sortNumber);
+    function sortNumber(a,b) {
+    return a - b;
+    }
+
+    function output(sortedArray){
+        var arrAsString = sortedArray.join(";  ");
+        getClass(".result__output").innerHTML = arrAsString;
+    }
+
+    /** for convenience) 
+    accepts receivedClass as ".className"*/   /*
+    var getClass = function(receivedClass){
+        var dd =  document.querySelector(receivedClass);
+        console.log(dd);
+        return dd;
+    }
+    
+*/
+    document.writeln("<p>bubble sort</p>");
+    document.writeln(Sorter.bubbleSort(arr));
+    document.writeln("<br>" + Sorter.insertionSort(arr));
