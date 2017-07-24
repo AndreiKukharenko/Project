49dc1f5ff9f63f62b9b59ec8f7f3ebc38f893d6d
Andrei Kukharenko <andrei-kukharenko@mail.ru>
2017-07-24 18:12:02 +0300

    3.4 fixed search in nested arrays. Little refactoring is required

--- 3/3.4/js/DFS.js
+++ 3/3.4/js/DFS.js
@@ -1,14 +1,27 @@
 (function(global){
     'use strict';
     var dfs = { 
-        depthFirst : function(obj, find, resultParent){
+        depthFirst : function(obj, find, resultParent, pathParent){
 
             var result = resultParent || [];
-
             if(obj instanceof Array && !(obj instanceof String) ) {
             for(var i = 0; i < obj.length; i++) {
-                if ((obj[i]+"").search(find) >= 0) result.push(obj[i]);
-                this.depthFirst(obj[i], find, result);
+                var path = pathParent || "";
+                path = path +"[" + i + "].";
+
+               var createDescriptionOf = function(typeEl){
+                    let element = new Object();
+                    element['value'] = obj[i];
+                    element['path'] = path;
+                    return JSON.stringify(element);
+                }
+
+                
+                if (!(obj[i] instanceof Array) && !(obj[i] instanceof Object)
+                    && (obj[i]+"").search(find) >= 0) {
+                        result.push(createDescriptionOf (obj[i]));
+                }
+                this.depthFirst(obj[i], find, result, path);
                 }
             }
             else{
@@ -16,15 +29,24 @@
                 for(let index = 0; index < keys.length; index++) {
                     let key = keys[index];
                     let value = obj[key];
-
+                    var path = pathParent || "";
+                    var createDescriptionOf = function(typeEl){
+                        let element = new Object();
+                        element['key'] = key;
+                        element['value'] = value;
+                        element['type'] = typeEl;
+                        element['path'] = path;
+                        return JSON.stringify(element);
+                    }
+                    path = path + key+".";
                     if ((key + "").search(find) >= 0){
-                        result.push(key);  
+                        result.push(createDescriptionOf("key"));  
                     }  
                     if (!(value instanceof Object) && (value+"").search(find) >= 0){
-                        result.push(value);
+                        result.push(createDescriptionOf("value"));
                     } 
                     if(value instanceof Object || value instanceof Array){
-                        this.depthFirst(value, find, result);
+                        this.depthFirst(value, find, result, path);
                     }
                 }
             }
--- 3/3.4/js/script.js
+++ 3/3.4/js/script.js
@@ -6,28 +6,18 @@ var car = {
   wheels: 4,
   engine: {
     cylinders: {
-      count : 4, //engine.cylinders.count
+      count : 4, 
       radius : {
         76 : 90
       },
       height : [86, 8, 8]
     },
-    size: 2.2
+    size: 2.2,
+    new : [12,'23ii423', {aiibc:10}, ['test', 'piing']],
   },
 
   state: "stopped"
 };
 
-document.writeln(DFS.depthFirst(car, "6"));
+document.writeln(DFS.depthFirst(car, "ii"));
 document.writeln(DFS.depthFirst(car, "o"));
-
-
-            /*[{
-              key:'abc'.
-              value:'78',
-              type:'key'  
-           },{
-              key:'ac',
-              value:'78b',
-              type:'value'  
-           }]*/
