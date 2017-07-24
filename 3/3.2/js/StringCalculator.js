a209dff5422cc2076df9d9062b54847126a1db74
Andrei Kukharenko <andrei-kukharenko@mail.ru>
2017-07-24 18:46:17 +0300

    3.2 fixed to prevent passing wrong symbols in arguments

--- 3/3.2/js/StringCalculator.js
+++ 3/3.2/js/StringCalculator.js
@@ -1,13 +1,23 @@
 (function(global){
+    "use strict";
+    var strCalc;
     strCalc = {
         calculate : function(str){
-            var result;
-            var regex = '[a-zA-Z;\]'; //anti-hack)
+            var regex = '[a-zA-Z;\]'; //prevent any js code as an argument)
+            if (str.match(regex)) return NaN; // error : string is not a math expression!
             str = str.replace(/(,)/g , '.'); // supports commas as well
-            if (str.match(regex)) return -1; // error : string is not a math expression!
-            result = new Function('return ' + str)();
-            return result;
-        },
-    }
+            str = str.replace(/( )/g , ''); // fix empty spaces
+
+            try {
+                var res = new Function('return ' + str);
+                res();
+            }
+            catch(ReferenceError) { 
+                //catch if there are ANY wrong symbols (other alphabet, for example)
+                return NaN;
+            }
+            return new Function('return ' + str)();
+        }
+    };
     global.StrCalc = strCalc;
-})(this)
+})(this);
--- 3/3.2/js/script.js
+++ 3/3.2/js/script.js
@@ -2,7 +2,7 @@
 document.writeln("<p>Calculator supports brackets and commas</p>");
 var str = "((15+2.5)/5+19)";
 
-var str2 = "330/(12.6+2,4)-15"
+var str2 = "console.log(33)"
 
 document.writeln(StrCalc.calculate(str)+ "<br></br>");
 document.writeln(StrCalc.calculate(str2));
