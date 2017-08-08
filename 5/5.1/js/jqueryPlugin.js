8149a3016d79fa4c6c2d2dbcd2c31f4e2e819710
Andrei Kukharenko <andrei-kukharenko@mail.ru>
2017-08-08 14:16:47 +0300

    * 5.1 separated method's responsibility

--- 5/5.1/js/jqueryPlugin.js
+++ 5/5.1/js/jqueryPlugin.js
@@ -1,39 +1,6 @@
 (function($){
     "use strict";
     $.fn.dragAndDropPlugin = function(){
-        var images;
-        function sendFormToServer(formData){
-            var uploadURL ="http://httpbin.org/post"; //Upload URL
-            var jqXHR = $.ajax({
-                url: uploadURL,
-                type: "POST",
-                contentType: false,
-                processData: false,
-                cache: false,
-                data: formData,
-                async: false //deprecated. only to avoid error when request goes to httpbin.org/post 
-            }).done(function(data, textStatus, jqXHR){
-                $(".success").show();
-            }).fail(function(jqXHR, textStatus, errorThrown){
-                alert("upload " + textStatus)
-            });
-        }
-
-        function handleFormUpload(files,obj){
-            var fd = new FormData();
-            var login = $(".form-control__login").val();
-            var password = $(".form-control__password").val();
-            fd.append("login", login);
-            fd.append("password", password);
-            if(files){
-                for (var i = 0; i < files.length; i++){
-                    fd.append("img" + i, files[i]);
-                }
-                if(login && password){
-                    sendFormToServer(fd);
-                }
-            }
-        }
 
         $(document).ready(function(){
             var obj = $(".dragandrophandler");
@@ -46,8 +13,8 @@
             obj.on("drop", function(e){
                 $(this).css("border", "2px dotted #0B85A1");
                 e.preventDefault();
-                images = e.originalEvent.dataTransfer.files;
-                handleImagePreview(images);
+                $.fn.images = e.originalEvent.dataTransfer.files;
+                handleImagePreview($.fn.images);
             });
 
             obj.on("dragover", function(e){
@@ -73,27 +40,18 @@
         });
 
         function handleImagePreview(input){
-                var filesAmount = input.length;
-                for (let i = 0; i < filesAmount; i++){
-                    var reader = new FileReader();
-                    reader.onload = function(event){
-                        var $img = $("<img>", {
-                            src: event.target.result
-                        });
-                        $img.appendTo(".preview");
-                    }
-                    reader.readAsDataURL(input[i]);
+            var filesAmount = input.length;
+            for (let i = 0; i < filesAmount; i++){
+                var reader = new FileReader();
+                reader.onload = function(event){
+                    var $img = $("<img>", {
+                        src: event.target.result
+                    });
+                    $img.appendTo(".preview");
                 }
-        };
-        
-        $(".button__submit--register").click(function(e){
-            if(images){
-                handleFormUpload(images);
-            }else{
-                alert("please attach images to the form");
+                reader.readAsDataURL(input[i]);
             }
-            e.preventDefault();
-        });
+        };
     }
 })(jQuery);
 
--- 5/5.1/js/script.js
+++ 5/5.1/js/script.js
@@ -1,12 +1,57 @@
 (function(global){
   "use strict";
+    var registrForm = ".registration";
+    var successMsgSelector = ".success";
+
     $(document).ready(
       function(){
-          $().dragAndDropPlugin();
+        $().dragAndDropPlugin();
+
+        function sendFormToServer(formData){
+            var uploadURL ="http://httpbin.org/post"; //Upload URL
+            var jqXHR = $.ajax({
+                url: uploadURL,
+                type: "POST",
+                contentType: false,
+                processData: false,
+                cache: false,
+                data: formData,
+                async: false //deprecated. only to avoid error when request goes to httpbin.org/post 
+            }).done(function(data, textStatus, jqXHR){
+                $(".success").show();
+            }).fail(function(jqXHR, textStatus, errorThrown){
+                alert("upload " + textStatus)
+            });
+        }
+
+        function handleFormUpload(images){
+            var fd = new FormData();
+            var login = $(".form-control__login").val();
+            var password = $(".form-control__password").val();
+            fd.append("login", login);
+            fd.append("password", password);
+            if(images){
+                for (var i = 0; i < images.length; i++){
+                    fd.append("img" + i, images[i]);
+                }
+                if(login && password){
+                    sendFormToServer(fd);
+                }
+            }else{
+                alert("please attach images to the form");
+            }
+        }
+
+        $(".button__submit--register").click(function(e){
+            let images = $.fn.images;
+            handleFormUpload(images);
+            e.preventDefault();
+        });
+
 
           $(".button__submit--Ok").click(function(){
-              $(".success").hide();
-              resetForm($(".registration"));
+              $(successMsgSelector).hide();
+              resetForm($(registrForm));
           });
 
           function resetForm($form) {
